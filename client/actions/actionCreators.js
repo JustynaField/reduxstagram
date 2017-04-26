165cc8c6781d115ea9df0075cd3dee0ca6c1aeef
Justyna Field <justyna.field@gmail.com>
2017-04-26 13:54:24 -0600

    first commit

---
+++ client/actions/actionCreators.js
@@ -0,0 +1,30 @@
+// increment (likes)
+export function increment(index) {
+  return {
+    type: 'INCREMENT_LIKES',
+    index
+  }
+}
+//index is an identifier for which post needs to be updated
+
+
+// add comment
+export function addComment(postId, author, comment) {
+  console.log('Dispatching comment')
+  return {
+    type: 'ADD_COMMENT',
+    postId,
+    autor,
+    comment
+  }
+}
+
+// remove comment
+export function removeComment(postId, i) {
+  return {
+    type: 'REMOVE_COMMENT',
+    i,
+    postId
+  }
+}
+//postId - which post are working with / i - what comment should be actually removed
